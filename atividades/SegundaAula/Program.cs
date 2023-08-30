407d6834f853cea1cc08f507e92cfa2db9f0e177
laiscastilho <laynefirmino123@gmail.com>
2023-08-30 20:33:59 -0300

    Primeiro Commit

---
+++ atividades/SegundaAula/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+
+string[] months = new string[12];
+
+for (int month = 1; month <= 12; month++)
+{
+    DateTime firstDay = new DateTime(DateTime.Now.Year, month, 1);
+}
---
+++ atividades/exe01/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int[] numeros = new int[10] { 34, 9, 234, 67, 32, 8, 2, 33, 888, 1};
+
+Array.Sort(numeros);
+Console.WriteLine($"{numeros[0]}");
+Console.WriteLine($"{numeros[1]}");
+Console.WriteLine($"{numeros[2]}");
+Console.WriteLine($"{numeros[3]}");
+Console.WriteLine($"{numeros[4]}");
+Console.WriteLine($"{numeros[5]}");
+Console.WriteLine($"{numeros[6]}");
+Console.WriteLine($"{numeros[7]}");
+Console.WriteLine($"{numeros[8]}");
+Console.WriteLine($"{numeros[9]}");
